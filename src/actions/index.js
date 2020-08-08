ef6592fced9d5ca6515a9b7d13ff3d3ce5970023
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-08 18:52:50 +1000

    almost completed the app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
