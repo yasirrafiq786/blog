b26284bb7aa676a4c690892d5512fae25568e958
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-08 11:22:11 +1000

    rendered posts on page

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,15 +3,35 @@ import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
 class PostList extends React.Component {
-componentDidMount() {
+  componentDidMount() {
     this.props.fetchPosts();
-}
+  }
+
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
 
   render() {
-    return <div>PostList</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
-export default connect(null, {
+const mapStateToProps = (state) => {
+  return {posts: state.posts};
+};
+
+export default connect(mapStateToProps, {
   fetchPosts,
 })(PostList);
