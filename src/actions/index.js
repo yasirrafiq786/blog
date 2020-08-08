b17c003e706c791d6a1a3559d7204c738d18403e
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-08 18:39:00 +1000

    added another action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,18 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  await dispatch(fetchPosts());
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({type: 'FETCH_USER', payload: response.data});
-});
+};
