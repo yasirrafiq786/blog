de1466e2eb1692563a8dc793444267b81faeb457
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-08 12:18:37 +1000

    Made some refactoring in UserHeader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,8 +8,17 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    return <div>User</div>;
+    const {user} = this.props;
+
+    if (!user) {
+      return null;
+    }
+    return <div className="header">{user.name}</div>;
   }
 }
 
-export default connect(null, {fetchUser})(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+  return {user: state.users.find((user) => user.id === ownProps.userId)};
+};
+
+export default connect(mapStateToProps, {fetchUser})(UserHeader);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux';
 import postsReducer from './postsReducer';
-import userReducer from './usersReducer';
 import usersReducer from './usersReducer';
 
 export default combineReducers({
